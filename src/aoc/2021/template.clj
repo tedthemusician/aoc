58cd0b3f7d91cc177097bb2f333c778f3e1f8992
Ted Oliver <tedsoliver@tedsoliver.com>
2021-12-04 20:36:37 -0600

    Clean up template

--- src/aoc/2021/template.clj
+++ src/aoc/2021/template.clj
@@ -1,7 +1,7 @@
 (ns aoc.2021.26
   (:require [clojure.string :as str]
             [clojure.edn :as edn]
-            [clojure.pprint :as pprint]
+            [clojure.pprint :refer [pprint]]
             [aoc.utils :as utils])
   (:gen-class))
 
@@ -17,7 +17,7 @@
 
 (utils/show-results
   ; Add an :input key to verify a puzzle input's expected output
-  [{:method solve-1 :sample :s1 :input :i1}
+  [{:method solve-1 :sample :s1}
    {:method solve-2 :sample :s2}]
   {:value sample}
   (utils/get-read-lines 2021 26))
