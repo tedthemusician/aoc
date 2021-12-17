aafef9c40070d4834e86bb7bfa756c7f57251b88
Ted Oliver <tedsoliver@tedsoliver.com>
2021-12-16 21:52:18 -0600

    Add 2021 day 15 part 2 (probably O(2^n). Stay away.)

--- src/aoc/2021/15.clj
+++ src/aoc/2021/15.clj
@@ -17,6 +17,8 @@
              [1 2 9 3 1 3 8 5 2 1]
              [2 3 1 1 9 4 4 5 8 1]])
 
+(def nvisits (atom 0))
+
 (defn getm
   [m {:keys [x y]}]
   (nth (nth m y) x))
@@ -36,15 +38,21 @@
    :value nil
    :prev nil})
 
-(defn init [m]
-  (let [width (count (first m))
-        height (count m)
-        source (vec (flatten m))]
+(defn init [m mul]
+  (let [source (vec (flatten m))
+        source-width (count (first m))
+        source-height (count m)
+        width (* source-width mul)
+        height (* source-height mul)]
     {:source source
+     :source-width source-width
+     :source-height source-height
      :width width
      :height height
      :end {:x (dec width) :y (dec height)}
      :visited #{}
+     :to-visit (into (sorted-map) {1 {:x 0 :y 0}})
+     ; Add when less than some threshold?
      :known {{:x 0 :y 0} {:weight (first source)
                           :value (first source)
                           :prev nil}}}))
@@ -65,13 +73,8 @@
           (get-neighbors point)))
 
 (defn point->index
-  [{:keys [width]} {:keys [x y]}]
-  (+ x (* width y)))
-
-(defn index->point
-  [{:keys [width]} i]
-  {:x (mod i width)
-   :y (quot i width)})
+  [{:keys [source-width]} {:keys [x y]}]
+  (+ x (* source-width y)))
 
 (defn visited?
   [mdata point]
@@ -81,11 +84,24 @@
   [mdata point]
   (update-in mdata [:visited] #(conj % point)))
 
+(defn adjust-value
+  [x offset]
+  (nth (iterate (fn [x'] (if (= x' 9) 1 (inc x'))) x) offset))
+
 (defn get-point-data
-  [mdata point]
+  [{:keys [source-width source-height known source]:as mdata}
+   {:keys [x y] :as point}]
   (if-let [existing (get-in mdata [:known point])]
     existing
-    (assoc default-point-data :value (nth (:source mdata) (point->index mdata point)))))
+    (let [wrapped-x (mod x source-width)
+          wrapped-y (mod y source-height)
+          x-level (quot x source-width)
+          y-level (quot y source-height)
+          source-point {:x wrapped-x :y wrapped-y}
+          source-value (nth source (point->index mdata source-point))
+          offset (+ x-level y-level)
+          adjusted-value (adjust-value source-value offset)]
+      (assoc default-point-data :value adjusted-value))))
 
 (defn set-point-data
   [mdata point pdata]
@@ -99,8 +115,6 @@
       (set-point-data mdata neighbor {:weight weight
                                       :prev point})
       mdata)))
-
-(def md (init sample))
 
 (defn visit
   [mdata point]
@@ -120,7 +134,13 @@
 
 (defn visit-lightest-known
   [mdata]
-  (visit mdata (get-lightest-known mdata)))
+  (do
+    (swap! nvisits inc)
+    (print "\r")
+    (if (zero? (mod @nvisits 1000))
+      (print (str (quot @nvisits 1000) "k")))
+    (flush)
+    (visit mdata (get-lightest-known mdata))))
 
 (defn searching?
   [{:keys [visited end]}]
@@ -131,20 +151,37 @@
   (let [iterations (iterate visit-lightest-known mdata)]
     (first (drop-while searching? iterations))))
 
-(defn solve-1
-  [m]
-  (let [mdata (init m)
+(defn retrace
+  [mdata]
+  (let [end (:end mdata)]
+    (loop [current end, path (list)]
+      (let [pdata (get-point-data mdata current)
+            path' (conj path pdata)]
+        (if-let [prev (:prev pdata)]
+          (recur prev path')
+          path')))))
+
+(defn solve
+  [m mul]
+  (let [mdata (init m mul)
         origin-value (:value (get-point-data mdata {:x 0 :y 0}))
         {:keys [known]} (visit-all-until-end mdata)
         destination-weight (:weight (get known (:end mdata)))]
+    (println)
+    (println "!")
+    (swap! nvisits (constantly 0))
     (- destination-weight origin-value)))
 
+(defn solve-1
+  [m]
+  (solve m 1))
+
 (defn solve-2
-  [x]
-  nil)
+  [m]
+  (solve m 5))
 
 (utils/verify-solutions
   [{:method solve-1 :sample 40 :input 717}
-   #_ {:method solve-2 :sample :s2}]
+   #_ {:method solve-2 :sample 315}]
   {:value sample}
-  (map parse-line (utils/get-lines 2021 15)))
+  #_ (map parse-line (utils/get-lines 2021 15)))
