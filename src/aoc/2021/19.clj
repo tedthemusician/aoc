fd43d8d097884f40abcbb97166169790455cb8b7
Ted Oliver <tedsoliver@tedsoliver.com>
2021-12-20 18:40:08 -0500

    Add 2021 day 19 part 2

--- src/aoc/2021/19.clj
+++ src/aoc/2021/19.clj
@@ -19,18 +19,18 @@
 
 (let [signs (combo/selections [1 -1] 3)
       directions (map (fn [indices signs]
-                  (map (fn [index sign]
-                         {:index index :sign sign})
-                       indices
-                       signs))
-                (repeat [0 1 2]) signs)
-      
+                        (map (fn [index sign]
+                               {:index index :sign sign})
+                             indices
+                             signs))
+                      (repeat [0 1 2]) signs)
+
       all-povs (mapcat (fn [pov]
-                     (take 4
-                           (iterate (fn [[x y z]]
-                                      [(update y :sign -) x z])
-                                    pov)))
-                   directions)]
+                         (take 4
+                               (iterate (fn [[x y z]]
+                                          [(update y :sign -) x z])
+                                        pov)))
+                       directions)]
   (def povs (distinct povs)))
 
 (defn move-pov
@@ -61,8 +61,8 @@
   [s1 s2]
   (let [[match] (keep (fn [index]
                         (if-let [distance (get-distance s1 (move-povs index s2))]
-                         {:pov index :distance distance}))
-                       (range 24))]
+                          {:pov index :distance distance}))
+                      (range 24))]
     (or match nil)))
 
 (defn add-distance
@@ -78,31 +78,56 @@
     (distinct (concat known moved))))
 
 (defn assoc-next-matches
-  [[s & ss :as scanners]]
-  (if (= 1 (count scanners))
-    scanners
-    (let [match-groups (group-by (partial get-vantage s) ss)
-          nils (get match-groups nil)
-          matches (seq (dissoc match-groups nil))
-          knowns (reduce merge-match s matches)
-          ]
-      (concat [knowns] nils))))
+  [data]
+  (let [{:keys [scanners distances]} data]
+    (if (= 1 (count scanners))
+      data
+      (let [[s & ss] scanners
+            match-groups (group-by (partial get-vantage s) ss)
+            nils (get match-groups nil)
+            matches (seq (dissoc match-groups nil))
+            new-distances (map (comp :distance first) matches)
+            knowns (reduce merge-match s matches)]
+        {:distances (concat distances new-distances)
+         :scanners (concat [knowns] nils)}))))
+
+(defn get-manhattan-distance
+  [[x1 y1 z1] [x2 y2 z2]]
+  (+ (Math/abs (- x1 x2))
+     (Math/abs (- y1 y2))
+     (Math/abs (- z1 z2))))
+
+(defn get-largest-distance
+  [distances]
+  (let [pairs (combo/combinations distances 2)
+        pair-distances (map (partial apply get-manhattan-distance) pairs)]
+    (apply max pair-distances)))
+
+(defn get-all-scanners
+  [groups]
+  (->> groups
+       (assoc {:distances [(list 0 0 0)]} :scanners)
+       (utils/fix assoc-next-matches)))
 
 (defn solve-1
   [groups]
   (->> groups
        (map parse-group)
-       (utils/fix assoc-next-matches)
+       get-all-scanners
+       :scanners
        first
        count))
 
 (defn solve-2
-  [x]
-  nil)
+  [groups]
+  (->> groups
+       (map parse-group)
+       get-all-scanners
+       :distances
+       get-largest-distance))
 
 (utils/verify-solutions
-  ; Add an :input key to verify a puzzle input's expected output
-  [{:method solve-1 :sample 79}
-   #_ {:method solve-2 :sample :s2}]
+  [{:method solve-1 :sample 79 :input 335}
+   {:method solve-2 :sample 3621 :output 10864}]
   {:value sample}
   (utils/get-line-groups 2021 19))
