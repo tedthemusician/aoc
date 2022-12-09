f20d9fa97a4cd8f1b3444073c19dd4b55c6a3849
Ted Oliver <tedsoliver@tedsoliver.com>
2022-12-09 15:30:43 -0600

    Solve 2022 day 9 general case

--- src/aoc/2022/09.clj
+++ src/aoc/2022/09.clj
@@ -6,14 +6,22 @@
             [clojure.pprint :refer [pprint]])
   (:gen-class))
 
-(def sample ["R 4"
-             "U 4"
-             "L 3"
-             "D 1"
-             "R 4"
-             "D 1"
-             "L 5"
-             "R 2"])
+(def samples [["R 4"
+               "U 4"
+               "L 3"
+               "D 1"
+               "R 4"
+               "D 1"
+               "L 5"
+               "R 2"]
+              ["R 5"
+               "U 8"
+               "L 8"
+               "D 3"
+               "R 17"
+               "D 10"
+               "L 25"
+               "U 20"]])
 
 (def dirs {"U" [0 1]
            "D" [0 -1]
@@ -28,63 +36,95 @@
     {:dir (dirs d)
      :magnitude (edn/read-string n)}))
 
-(def s (map parse-line sample))
-(def initial-line {:head [0 0], :tail [0 0]})
-
 (defn distance
-  "Get the 'distance', a la D&D, between two points"
-  [{:keys [head tail]}]
-  (let [[hx hy] head
-        [tx ty] tail]
+  "Get the 'distance', a la D&D, between two points, such that diagonal
+  distance is the longest lateral distance."
+  [leader follower]
+  (let [[hx hy] leader
+        [tx ty] follower]
     (max (utils/abs (- hx tx))
          (utils/abs (- hy ty)))))
 
-(def move-end (partial map +))
+(def add-vectors (partial mapv +))
 
-(defn pull-line-once
-  "Move head one unit in a given direction and ensure tail is one unit away"
-  [dir starting-line]
-  (let [starting-distance (distance starting-line)
-        pulled-line (update starting-line :head (partial move-end dir))]
-    (if (zero? starting-distance)
-      pulled-line
-      (let [pulled-distance (distance pulled-line)]
-        (if (<= pulled-distance 1)
-          pulled-line
-          (assoc pulled-line :tail (:head starting-line)))))))
+(defn get-change
+  "Get the change necessary to apply to follower in order to ensure it stays
+  next to leader"
+  [leader' follower]
+  (let [[dx dy] (mapv - leader' follower)]
+    [(Integer/signum dx) (Integer/signum dy)]))
 
-(defn pull-line-multiple
-  "Move head multiple units in a given direction and ensure tail is always one
-  unit away. Return the new position and all coordinates visited by tail."
-  [{:keys [line tail-coords]} {:keys [dir magnitude]}]
-  (let [intermediate-lines (rest (take (inc magnitude)
-                                  (iterate (partial pull-line-once dir)
-                                           line)))
-        new-line (last intermediate-lines)
-        tail-coords' (set (map :tail intermediate-lines))]
-    {:line new-line :tail-coords (set/union tail-coords tail-coords')}))
+(defn diagonal?
+  "Are two points diagonal from each other?"
+  [[x1 y1] [x2 y2]]
+  (and (not= x1 x2) (not= y1 y2)))
+
+(defn avg [a b] (/ (+ a b) 2))
+
+(defn pull-linear
+  "Pull follower toward leader along x or y axis"
+  [[x1 y1] [x2 y2]]
+  [(avg x1 x2) (avg y1 y2)])
+
+(defn pull
+  "Pull follower to meet leader' according to the distance between leader' and
+  leader such that follower stays one unit away. If leader' is more than one
+  unit away from follower:
+  - If leader and follower were originally diagonal, move follower to leader
+  - Otherwise, move follower in the same manner as leader-to-leader'"
+  [leader leader' follower]
+  (let [starting-distance (distance leader follower)]
+    (let [pulled-distance (distance leader' follower)]
+      (cond (<= pulled-distance 1) follower
+            (diagonal? leader' follower) (add-vectors follower (get-change leader' follower))
+            :otherwise (pull-linear follower leader')))))
+
+(defn drag-once
+  "Pull the head of a chain one unit horizontally, vertically, or digaonally,
+  and pull all following nodes one by one."
+  [dir [leader & remaining]]
+  (let [leader' (add-vectors leader dir)]
+    (loop [leader leader
+           leader' leader'
+           remaining remaining
+           new-points (list leader')]
+      (if (empty? remaining)
+        (reverse new-points)
+        (let [[follower & remaining'] remaining
+              follower' (pull leader leader' follower)
+              new-points' (cons follower' new-points)]
+          (recur follower follower' remaining' new-points'))))))
+
+(defn drag-multiple
+  "Drag head of chain multiple units. Return new node points and all points
+  visited by tail."
+  [{:keys [chain tail-coords]} {:keys [dir magnitude]}]
+  (let [intermediate-chains (rest (take (inc magnitude)
+                                  (iterate (partial drag-once dir) chain)))
+        new-chain (last intermediate-chains)
+        tail-coords' (set (map last intermediate-chains))]
+    {:chain new-chain :tail-coords (set/union tail-coords tail-coords')}))
 
 (defn follow-path
   "Pull a line along a path given by instructions"
-  [instructions]
-  (reduce pull-line-multiple {:line initial-line :tail-coords #{}} instructions))
+  [initial-chain instructions]
+  (reduce drag-multiple
+          {:chain initial-chain, :tail-coords #{[0 0]}}
+          instructions))
 
-(count (:tail-coords (follow-path s)))
-
-(defn solve-1
-  [input]
+(defn solve
+  [chain-len input]
   (->> input
        (map parse-line)
-       follow-path
+       (follow-path (repeat chain-len [0 0]))
        :tail-coords
        count))
 
-(defn solve-2
-  [input]
-  nil)
+(def solve-1 (partial solve 2))
+(def solve-2 (partial solve 10))
 
 (utils/verify-solutions
-  [{:method solve-1 :sample 13}
-   #_ {:method solve-2 :sample nil :input nil}]
-  {:value sample}
+  [{:method solve-1 :sample [13 88] :input 6209}
+   {:method solve-2 :sample [1 36] :input 2460}]
+  {:multiple samples}
   (utils/get-lines 2022 9))
