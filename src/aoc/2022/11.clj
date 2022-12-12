446037e28dc050150a8de7b302a66ab9d190f5fc
Ted Oliver <tedsoliver@tedsoliver.com>
2022-12-12 13:42:58 -0600

    Add 2022 day 11 part 2

--- src/aoc/2022/11.clj
+++ src/aoc/2022/11.clj
@@ -1,8 +1,7 @@
 (ns aoc.2022.11
   (:require [aoc.utils :as utils]
             [clojure.string :as str]
-            [clojure.edn :as edn]
-            [clojure.pprint :refer [pprint]])
+            [clojure.edn :as edn])
   (:gen-class))
 
 (def sample [["Monkey 0:"
@@ -53,9 +52,9 @@
 (defn parse-expression
   "Get the worry level expression associated with a monkey"
   [line]
-  (let [[_ t1 op t2] (re-find #"(\w+) (\+|\*) (\w+)$" line)]
+  (let [[_ op term] (re-find #"(\+|\*) (\w+)$" line)]
     {:expression {:op op
-                  :terms (map parse-term [t1 t2])}}))
+                  :term (parse-term term)}}))
 
 (defn parse-modulus
   "Get the modulus for a monkey's throw condition"
@@ -89,20 +88,21 @@
         monkey (reduce merge maps)]
     (assoc monkey :nticks 0)))
 
-(def s (mapv parse-monkey sample))
+(def parse-input (partial mapv parse-monkey))
 
 (defn apply-op-to-scalar
-  "Apply an operation to terms, substituting :old with n"
-  [n op terms]
-  (let [func (ops op)]
-    (apply func (map (fn [term] (if (= :old term) n term)) terms))))
+  "Apply an operation to n and term, substituting :old with n"
+  [op term n]
+  (let [func (ops op)
+        term' (if (= :old term) n term)]
+    (func term' n)))
 
 (defn update-scalar-worry-level
   "Update the worry level of the first item in a monkey's list of items"
   [{:keys [items expression] :as monkey}]
   (let [[item] items
-        {:keys [op terms]} expression
-        new-level (apply-op-to-scalar item op terms)]
+        {:keys [op term]} expression
+        new-level (apply-op-to-scalar op term item)]
     (assoc-in monkey [:items 0] (quot new-level 3))))
 
 (defn test-scalar-worry-level
@@ -111,13 +111,62 @@
   [{:keys [items modulus]}]
   (zero? (mod (first items) modulus)))
 
-(defn throw-next-scalar-item
+(defn convert-scalar-to-remainders
+  "Given a list of divisors, convert a scalar into a map from the divisor to
+  the remainder of the modulus of the scalar and the divisor"
+  [divisors scalar]
+  (zipmap divisors
+          (map #(mod scalar %) divisors)))
+
+(defn convert-monkey-scalars-to-remainder-maps
+  "Convert one monkey's items from scalars to maps of remainders"
+  [divisors monkey]
+  (update monkey :items (partial mapv
+                                 (partial convert-scalar-to-remainders
+                                          divisors))))
+
+(defn convert-all-scalars-to-remainder-maps
+  "Convert all monkeys' items from scalars to maps of remainders"
+  [monkeys]
+  (let [divisors (map :modulus monkeys)]
+    (mapv (partial convert-monkey-scalars-to-remainder-maps divisors) monkeys)))
+
+(defn apply-op-to-remainder
+  "Apply an operation to a term and a remainder"
+  [op term [divisor remainder]]
+  (let [func (ops op)
+        resolved-term (if (= term :old)
+                        remainder
+                        term)
+        remainder' (mod (func remainder resolved-term) divisor)]
+    {divisor remainder'}))
+
+(defn apply-op-to-remainders
+  "Apply an operation to a term and a remainder"
+  [op term remainders]
+  (reduce merge (map (partial apply-op-to-remainder op term) remainders)))
+
+(defn update-remainder-worry-level
+  "Update the worry level of the first item in a monkey's list of items"
+  [{:keys [items expression] :as monkey}]
+  (let [[item] items
+        {:keys [op term]} expression
+        new-level (apply-op-to-remainders op term item)]
+    (assoc-in monkey [:items 0] new-level)))
+
+(defn test-remainder-worry-level
+  "Test whether the divisor specified by a monkey's modulus is zero."
+  [{:keys [items modulus]}]
+  (let [remainder (get (first items) modulus)]
+    (zero? remainder)))
+
+(defn throw-next-item
   "Throw the first item from the monkey at monkey-index to its appropriate
   target"
-  [source-index monkeys]
-  (let [source-monkey (update-scalar-worry-level (nth monkeys source-index))
+  [config source-index monkeys]
+  (let [source-monkey ((:update config) (nth monkeys source-index))
         [thrown-item] (:items source-monkey)
-        default? (test-scalar-worry-level source-monkey)
+        default? ((:test config) source-monkey)
         target-key (if default? :default-target :alternative-target)
         target-index (get source-monkey target-key)]
     (-> monkeys
@@ -125,36 +174,46 @@
         (update-in [source-index :nticks] inc)
         (update-in [target-index :items] #(conj % thrown-item)))))
 
-(defn take-turn-with-scalars
+(defn take-turn
   "Throw all items from the monkey at monkey-index to their appropriate
   targets"
-  [monkeys source-index]
+  [config monkeys source-index]
   (let [source-monkey (nth monkeys source-index)
         nitems (count (:items source-monkey))]
-    (nth (iterate (partial throw-next-scalar-item source-index) monkeys) nitems)))
+    (nth (iterate (partial throw-next-item config source-index) monkeys) nitems)))
 
-(defn play-round-with-scalars
+(defn play-round
   "All monkeys throw all their items"
-  [monkeys]
-  (reduce take-turn-with-scalars monkeys (range (count monkeys))))
+  [config monkeys]
+  (reduce (partial take-turn config) monkeys (range (count monkeys))))
+
+(defn get-monkey-business
+  [final-state]
+  (->> final-state
+       (map :nticks)
+       sort
+       reverse
+       (take 2)
+       (apply *)))
 
 (defn solve-1
   [input]
-  (let [monkeys (mapv parse-monkey input)
-        final-state (nth (iterate play-round-with-scalars monkeys) 20)]
-    (->> final-state
-         (map :nticks)
-         sort
-         reverse
-         (take 2)
-         (apply *))))
+  (let [monkeys (parse-input input)
+        play-func (partial play-round {:update update-scalar-worry-level 
+                                       :test test-scalar-worry-level})
+        final-state (nth (iterate play-func monkeys) 20)]
+    (get-monkey-business final-state)))
 
 (defn solve-2
   [input]
-  ())
+  (let [monkeys (convert-all-scalars-to-remainder-maps (parse-input input))
+        play-func (partial play-round {:update update-remainder-worry-level
+                                       :test test-remainder-worry-level})
+        final-state (nth (iterate play-func monkeys) 10000)]
+    (get-monkey-business final-state)))
 
 (utils/verify-solutions
   [{:method solve-1 :sample 10605 :input 58786}
-   #_ {:method solve-2 :sample nil :input nil}]
+   {:method solve-2 :sample 2713310158 :input 14952185856}]
   {:value sample}
   (utils/get-groups 2022 11))
