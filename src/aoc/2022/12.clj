8e34b2d0d548d498623be3afdd80f2b64a4cb8b1
Ted Oliver <tedsoliver@tedsoliver.com>
2022-12-12 22:40:36 -0600

    Add 2022 day 12 part 1

---
+++ src/aoc/2022/12.clj
@@ -0,0 +1,135 @@
+(ns aoc.2022.12
+  (:require [aoc.utils :as utils]
+            [clojure.set :as set])
+  (:gen-class))
+
+(def sample ["Sabqponm"
+             "abcryxxl"
+             "accszExk"
+             "acctuvwj"
+             "abdefghi"])
+
+(defn parse-char
+  "Parse S as :start, E as :end, or any other lowercase letter as its distance
+  past lowercase a"
+  [c]
+  (case c
+    \S :start
+    \E :end
+    (- (int c) (int \a))))
+
+(defn find-point
+  "Find a point in a matrix whose value is label"
+  [m label]
+  (let [y (utils/find-index #((set %) label) m)
+        x (utils/find-index (partial = label) (nth m y))]
+    [x y]))
+
+(defn parse-input
+  "Parse input as a matrix of heights, a start point, and an end point"
+  [input]
+  (let [matrix (mapv (partial mapv parse-char) input)
+        start (find-point matrix :start)
+        end (find-point matrix :end)]
+    {:matrix (mapv (fn [row]
+                     (mapv (fn [cell]
+                             (case cell
+                               :start 0
+                               :end 25
+                               cell))
+                           row))
+                   matrix)
+     :start start
+     :end end}))
+
+(def s (parse-input sample))
+(def mat (:matrix s))
+
+(def adjustments [[-1 0] [0 -1] [1 0] [0 1]])
+
+(def add-points (partial mapv +))
+
+(def width (comp count first))
+
+(def height count)
+
+(defn get-points [m]
+  "Get all coordinates of a matrix"
+  (for [x (range (width m)) y (range (height m))] [x y]))
+
+(defn get-at
+  "Get the value of the cell of a matrix at a coordinate"
+  [m [x y]]
+  (get-in m [y x]))
+
+(defn inbounds?
+  "Is a coordinate within the bounds of a matrix?"
+  [m [x y]]
+  (and (>= x 0)
+       (>= y 0)
+       (< x (count (first m)))
+       (< y (count m))))
+
+(defn get-neighbor-points
+  "Get all points of a matrix that neighbor a given point"
+  [m point]
+  (let [neighbors (map (partial add-points point) adjustments)]
+    (filter (partial inbounds? m) neighbors)))
+
+(defn get-height-difference
+  "Get the height difference, i.e. the difference in value, between two points
+  in a matrix"
+  [m c1 c2]
+  (let [h1 (get-at m c1)
+        h2 (get-at m c2)]
+    (- h2 h1)))
+
+(defn get-reachable-neighbors
+  "Get the neighbors of a given point that differ by 1 or 0"
+  [m point]
+  (let [level (get-at m point)
+        neighbors (get-neighbor-points m point)]
+    (set (filter #(<= (get-height-difference m point %) 1) neighbors))))
+
+(defn initialize-connections
+  "Create a mapping of points to reachable neighbors"
+  [m]
+  (let [points (get-points m)
+        neighbors (map (partial get-reachable-neighbors m) points)]
+    (zipmap points neighbors)))
+
+(defn explore-farthest
+  "Find which unreached points can be reached by the farthest known points"
+  [connections distance-groups]
+  (let [known-points (apply set/union distance-groups)
+        active-points (last distance-groups)
+        reachable-points (set (mapcat (partial get connections) active-points))
+        new-points (set/difference reachable-points known-points)]
+    (conj distance-groups new-points)))
+
+(defn explore-forever
+  "Explore a matrix via its connections along all paths"
+  [connections origin]
+  (->> [#{origin}]
+       (iterate (partial explore-farthest connections))
+       (map last)
+       (take-while (partial seq))))
+
+(defn solve-1
+  [input]
+  (let [{:keys [matrix start end]} (parse-input input)
+        connections (initialize-connections matrix)
+        distance-groups (explore-forever connections start)]
+    (utils/find-index #(% end) distance-groups)))
+
+(solve-1 sample)
+
+(defn solve-2
+  [input]
+  ())
+
+(utils/verify-solutions
+  [{:method solve-1 :sample 31}
+   #_ {:method solve-2 :sample nil :input nil}]
+  {:value sample}
+  (utils/get-lines 2022 12))
